315ddeafadb73722a6c2e1e77af728086f55a96f
belen <belen@loadingplay.com>
2017-09-20 12:05:37 -0300

    cambios bodegas y checkout  gdf

--- projects/placare/static/js/config.js
+++ projects/placare/static/js/config.js
@@ -6,13 +6,13 @@
 'use strict';
 
 
-        var base_url = $.environmentVar(
-            'http://apibodegas.ondev.today/',
-            'http://apibodegas.ondev.today/',
-            'https://apibodegas.loadingplay.com/');
-        var checkout_url = $.environmentVar(
-            'http://betapay.loadingplay.com',
-            'http://betapay.loadingplay.com',
-            'https://pay.loadingplay.com');
-        var app_public = $.environmentVar(53,53,53);
-        var site_name = $.environmentVar('placare', 'placare', 'placare');
+    var base_url = $.environmentVar(
+        'http://apibodegas.ondev.today/',
+        'http://apibodegas.ondev.today/',
+        'https://apibodegas.loadingplay.com/');
+    var checkout_url = $.environmentVar(
+        'http://betapay.loadingplay.com',
+         'http://betapay.loadingplay.com',
+        'https://pay.loadingplay.com');
+    var app_public = $.environmentVar(53,53,53);
+    var site_name = $.environmentVar('placare', 'placare', 'placare');
