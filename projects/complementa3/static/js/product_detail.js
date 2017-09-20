7cb5af23161051ecb64810701ed685043ada9b9d
belen <belen@loadingplay.com>
2017-09-20 12:20:41 -0300

    cambios bodegas y checkout  gdf

--- projects/complementa3/static/js/product_detail.js
+++ projects/complementa3/static/js/product_detail.js
@@ -5,12 +5,12 @@ $(document).ready(function()
 {
     
     var base_url = $.environmentVar(
-        'http://apibodegas.ondev.today/',
-        'http://apibodegas.ondev.today/',
+        'https://apibodegas.loadingplay.com/',
+        'https://apibodegas.loadingplay.com/',
         'https://apibodegas.loadingplay.com/');
     var checkout_url = $.environmentVar(
-        'http://betapay.loadingplay.com',
-        'http://betapay.loadingplay.com',
+        'https://pay.loadingplay.com',
+        'https://pay.loadingplay.com',
         'https://pay.loadingplay.com');
     var app_public = $.environmentVar(15,15,15);
     var site_name = $.environmentVar('complementa', 'complementa', 'complementa');
