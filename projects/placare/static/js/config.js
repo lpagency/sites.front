057c4aeb6b63797647eb1446993b2361d85438da
belen <belen@loadingplay.com>
2017-09-21 12:01:25 -0300

    cambios enc en complementa

--- projects/placare/static/js/config.js
+++ projects/placare/static/js/config.js
@@ -14,4 +14,4 @@
             'http://lpcheckout.ondev.today',
             'https://betapay.loadingplay.com');
         var app_public = $.environmentVar(53,53,53);
-        var site_name = $.environmentVar('placare', 'placare', 'placare');
+        var site_name = $.environmentVar('placare', 'placare', 'placare'); 
