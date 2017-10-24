b4ed4fd6ccd923df24b3bde1f0da4485a21aea24
Lía Maldonado <evolninemuses@gmail.com>
2017-10-24 17:01:59 -0300

    como estaba! ltutty

--- projects/luttyflores2/static/js/config.js
+++ projects/luttyflores2/static/js/config.js
@@ -10,8 +10,8 @@
           'https://apibodegas.loadingplay.com/',
           'https://apibodegas.loadingplay.com/');
     var checkout_url = $.environmentVar(
-        'https://betapay.loadingplay.com');
-        'https://betapay.loadingplay.com');
-        'https://betapay.loadingplay.com');
+        'https://checkout.loadingplay.com',
+        'https://checkout.loadingplay.com',
+        'https://checkout.loadingplay.com');
     var app_public = $.environmentVar(48,48,48);
     var site_name = $.environmentVar('luttyflores', 'luttyflores', 'luttyflores');
