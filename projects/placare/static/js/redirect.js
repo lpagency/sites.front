922daa79068449d47581553ac257afd8ca15a52d
Wayo Carrasco <eduardo.carrasco.nz@gmail.com>
2017-11-02 15:19:54 -0300

    redireccionar placare.com/mujeres a listado_Productos

---
+++ projects/placare/static/js/redirect.js
@@ -0,0 +1,7 @@
+$(document).ready(function()
+{
+	setTimeout(function()
+	{
+		window.location.replace("http://www.placare.com/listado_productos?tag=Categoria2_Calzado_Mujer,-Categoria2_Calzado_Hombre");
+	},1000);
+});
