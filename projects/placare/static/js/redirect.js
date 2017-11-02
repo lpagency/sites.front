6d4e887c546f64faca2332cf4bf237bab927cdc4
Wayo Carrasco <eduardo.carrasco.nz@gmail.com>
2017-11-02 15:40:40 -0300

    redirect placare.com/hombres a listado_productos

--- projects/placare/static/js/redirect.js
+++ projects/placare/static/js/redirect.js
@@ -1,7 +1,21 @@
 $(document).ready(function()
 {
+	var url = "http://www.placare.com/listado_productos?tag=";
+	var hyper = window.location.href;
+	var path = "";
+
+	if(hyper.indexOf("mujeres") > -1)
+	{
+		path = "Categoria2_Calzado_Mujer,-Categoria2_Calzado_Hombre";
+	}
+
+	if(hyper.indexOf("hombres") > -1)
+	{
+		path = "Categoria2_Calzado_Hombre,-Categoria2_Calzado_Mujer";
+	}
+
 	setTimeout(function()
 	{
-		window.location.replace("http://www.placare.com/listado_productos?tag=Categoria2_Calzado_Mujer,-Categoria2_Calzado_Hombre");
+		window.location.replace(url + path);
 	},1000);
 });
