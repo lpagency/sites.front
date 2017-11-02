078713bf4df32ea671039be580bd17ce940a9453
belen <belen@loadingplay.com>
2017-11-02 14:18:35 -0300

    archivos base landing cerro castillo arreglo lutty flores

---
+++ projects/cerrocastillo/static/js/config.js
@@ -0,0 +1,16 @@
+/* global $ */
+/* global Utils */
+/* global unescape */
+/* global document */
+/* export config */
+'use strict';
+
+var base_url = $.environmentVar(
+    'https://apibodegas.loadingplay.com/',
+    'https://apibodegas.loadingplay.com/',
+    'https://apibodegas.loadingplay.com/');
+var checkout_url = $.environmentVar(
+    'https://checkout.loadingplay.com',
+    'https://checkout.loadingplay.com',
+    'https://checkout.loadingplay.com');
+var app_public = $.environmentVar(48,48,48);
