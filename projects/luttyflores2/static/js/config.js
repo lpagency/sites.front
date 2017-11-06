5a385a574aecd5190acf8a286c319b37cd5964e8
Wayo Carrasco <eduardo.carrasco.nz@gmail.com>
2017-11-06 15:46:49 -0300

    bodega de luttyflores

--- projects/luttyflores2/static/js/config.js
+++ projects/luttyflores2/static/js/config.js
@@ -13,5 +13,5 @@
         'http://lpcheckout.ondev.today',
         'https://betapay.loadingplay.com',
         'https://betapay.loadingplay.com');
-    var app_public = $.environmentVar(65,61,61);
+    var app_public = $.environmentVar(65,48,48);
     var site_name = $.environmentVar('luttyflores', 'luttyflores', 'luttyflores');
