97f8517bac3557ead1057d0b6d54cad3b3da981f
Wayo Carrasco <eduardo.carrasco.nz@gmail.com>
2017-11-06 15:50:53 -0300

    sincro placare

--- projects/placare/static/js/filtros_h.js
+++ projects/placare/static/js/filtros_h.js
@@ -21,7 +21,7 @@ $(document).ready(function()
         'https://betaapi.loadingplay.com/');
     var checkout_url = $.environmentVar(
         'http://lpcheckout.ondev.today',
-        'http://lpcheckout.ondev.today',
+        'https://betapay.loadingplay.com',
         'https://betapay.loadingplay.com');
     var app_public = $.environmentVar(53,53,53);
     var site_name = $.environmentVar('placare', 'placare', 'placare');
