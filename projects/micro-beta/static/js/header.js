fef547a9241bdf59b5ad0fba3c3bc6468f70d58d
belen <belen@loadingplay.com>
2017-11-10 17:32:41 -0300

    reversa cambios de micro y creacion micro-beta

---
+++ projects/micro-beta/static/js/header.js
@@ -0,0 +1,67 @@
+var HeaderView = function(controller)
+{
+    this.controller = controller;
+    this.is_running_animation = false;
+};
+
+HeaderView.prototype.animationInit = function() 
+{
+    var self = this;
+    $(window).scroll(function()
+    {
+        var scroll = $(window).scrollTop();
+        if (!self.is_running_animation)
+        {
+            if (scroll >= 60) 
+            {
+                if ($("#main-nav-fixed-navigation").css("margin-top") != "0px")
+                {
+                    self.is_running_animation = true;
+                    $("#main-nav-fixed-navigation").animate(
+                        {
+                            marginTop: 0
+                        },
+                        "slow",
+                        function() {
+                            self.is_running_animation = false;
+                        });
+                }
+            } 
+            else 
+            {
+                self.is_running_animation = true;
+                $("#main-nav-fixed-navigation").animate(
+                    {
+                        marginTop: -150
+                    },
+                    "fast",
+                    function()
+                    {
+                        self.is_running_animation = false;
+                    });
+            }
+        }
+    });
+};
+
+
+var HeaderController = function()
+{
+    this.view = new HeaderView();
+    this.view.animationInit();
+};
+
+
+$(document).ready(function()
+{
+    header_controller = new HeaderController();
+
+    // dropdown language
+    $('.lang-dropdown-menu').css('display', 'none');
+    $(document).on('click', '.lang-dropdown', function(e)
+    {
+        e.preventDefault();
+        // $('.lang-dropdown-menu').toggle();
+        $('.lang-modal').modal('show');
+    });
+});
