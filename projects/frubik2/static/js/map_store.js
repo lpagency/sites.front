80de49275bc5c17e97f79e136b4112d407bd6357
Chien <chienhung.lin@usach.cl>
2017-11-16 18:11:06 -0300

    frubik2 - corregir betaapi

--- projects/frubik2/static/js/map_store.js
+++ projects/frubik2/static/js/map_store.js
@@ -1,9 +1,9 @@
 
 
 var base_url = $.environmentVar(
-    'https://betabodegas.loadingplay.com/',
-    'https://betabodegas.loadingplay.com/',
-    'https://betabodegas.loadingplay.com/');
+    'https://betaapi.loadingplay.com/',
+    'https://betaapi.loadingplay.com/',
+    'https://betaapi.loadingplay.com/');
 var checkout_url = $.environmentVar(
      'https://betapay.loadingplay.com',
      'https://betapay.loadingplay.com',
