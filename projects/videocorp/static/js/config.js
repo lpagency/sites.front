22883e3837fc7985c7b0812ea4b12000eeb06bd1
anilentp <anilen.tp@gmail.com>
2017-12-15 13:45:37 -0300

    cambios videocorp

--- projects/videocorp/static/js/config.js
+++ projects/videocorp/static/js/config.js
@@ -5,12 +5,13 @@
 /* export config */
 'use strict';
 
-var base_url = $.environmentVar(
-    'https://apibodegas.loadingplay.com/',
-    'https://apibodegas.loadingplay.com/',
-    'https://apibodegas.loadingplay.com/');
-var checkout_url = $.environmentVar(
-    'https://pay.loadingplay.com',
-    'https://pay.loadingplay.com',
-    'https://pay.loadingplay.com');
-var app_public = $.environmentVar(29,29,29);
+    var base_url = $.environmentVar(
+        'https://apibodegas.loadingplay.com/',
+        'https://apibodegas.loadingplay.com/',
+        'https://apibodegas.loadingplay.com/');
+    var checkout_url = $.environmentVar(
+        'https://pay.loadingplay.com',
+        'https://pay.loadingplay.com',
+        'https://pay.loadingplay.com');
+    var app_public = $.environmentVar(60,60,60);
+    var site_name = $.environmentVar('videocorp', 'videocorp', 'videocorp'); 
