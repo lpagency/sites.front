7adb48180e02be82ff7d18af95d35aa0bc1d7989
Wayo Carrasco <eduardo.carrasco.nz@gmail.com>
2017-12-26 15:22:40 -0300

    biluppo ñ por n

--- projects/bilupo/static/js/cart.config.js
+++ projects/bilupo/static/js/cart.config.js
@@ -51,6 +51,7 @@ $(document).ready(function()
     var filtro = $(".filtrosMensaje").val();
     var filtro2 = $(".filtrosMensaje2").val();
     var filtro3 = $(".filtrosMensaje3").val();
+    filtro = filtro.replace("ñ","n");
     filtro = filtro.replace(" ","_");
     filtro = filtro.replace("-","_");
     filtro = filtro.replace("/","_");
